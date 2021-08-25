b9cdcfad39d4326810a1e9b4c8ab40fe5adeca99
IT21002038 <88493366+IT21002038@users.noreply.github.com>
2021-08-25 11:48:32 +0000

    I have completed exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
-  
+  float mrk1;
+  float mrk2;
+  float avg;
+
+  printf("enter subject 1 mark ");
+  scanf("%f",&mrk1);
+
+  printf("enter subject 2 mark");
+  scanf("%f",&mrk2);
+
+  avg=(mrk1+mrk2)/2;
+
+  printf("avarage mark is :%f",avg);
+
   return 0;
 }
 
